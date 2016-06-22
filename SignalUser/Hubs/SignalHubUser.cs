066494e14b53bea640e7f698b6d80651fa085d92
Vladislav Mayorov <mayorovvladislav@outlook.com>
2016-06-22 14:07:41 +0300

    Добавьте файлы проекта.

---
+++ SignalUser/Hubs/SignalHubUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalUser.Models;
+
+namespace SignalUser.Hubs
+{
+    public class MessageHubUser : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.messagesend(message);
+        }
+    }
+}
